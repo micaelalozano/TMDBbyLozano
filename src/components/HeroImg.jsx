d33b3f666241ff8b874b5a8005be98d0ca8c07e8
micaelalozano <micaelalozano.16@hotmail.com>
2023-04-24 21:43:07 -0300

    Adding latest Update to Git

--- src/components/HeroImg.jsx
+++ src/components/HeroImg.jsx
@@ -3,7 +3,8 @@ import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { Spinner } from "./Spinner";
-import AddPhotoAlternateOutlinedIcon from "@mui/icons-material/AddPhotoAlternateOutlined"; //Estilos
+import AddPhotoAlternateOutlinedIcon from "@mui/icons-material/AddPhotoAlternateOutlined"; 
+//Estilos
 import "../estilos/heroimg.css";
 
 const HeroImg = () => {
@@ -40,7 +41,7 @@ const HeroImg = () => {
     return <Spinner />;
   }
 
-  console.log("VER!", user);
+  console.log("VER!", username);
 
   return (
     <>
@@ -92,35 +93,3 @@ const HeroImg = () => {
 };
 
 export default HeroImg;
-
-/* {
-
-<div className="left-detalle">
-                      <img
-                        className="img1"
-                        src={
-                          e.imagen
-                            ? e.imagen
-                            : "https://thinktecno.com/wp-content/uploads/2022/06/Como-quitar-la-foto-de-perfil-en-Facebook-sin-borrar.png"
-                        }
-                        alt=""
-                      />
-                    </div>
-                    <div className="right-detalle">
-                      <h3 className="mi_cuenta">MI CUENTA</h3>
-                      <p className="mis_datos">NOMBRE: {e.name} </p>
-                      <p className="mis_datos">APELLIDO: {e.lastname} </p>
-                      <p className="mis_datos">
-                        NOMBRE DE USUARIO: {e.username}{" "}
-                      </p>
-                      <p className="mis_datos">CORREO ELECTRONICO: {e.email}</p>
-                      <div>
-                        <Link to={"/cambiar_foto_perfil/" + e.id}>
-                          <button className="btn-foto-perfil">
-                            Cambiar foto de perfil
-                          </button>
-                        </Link>
-                      </div>
-                    </div>
-
-} */
